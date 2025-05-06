6ed61727a982c2969bd757da619a2e845f830a4f
SanghyeonLee <hyeon012366@gmail.com>
2025-05-06 20:13:16 +0900

    feat: 서버 컴포넌트에서 get 요청

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
 // app/page.tsx
 import HomeTemplate from '@/components/templates/HomeTemplate';
+import { fetcher } from '@/lib/api/fetcher';
+import { API_URLS } from '@/constants/apiUrls';
+import { Post } from '@/types/domain/post';
 
-export default function Home() {
-  return <HomeTemplate />;
+export default async function Home() {
+  try {
+    const posts = await fetcher.post<Post[]>(API_URLS.POST.ALL, {});
+    return <HomeTemplate posts={posts} />;
+  } catch (err) {
+    console.error('❌ 에러:', err);
+    return <div>서버 에러 발생</div>;
+  }
 }
