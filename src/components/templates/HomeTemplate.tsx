0fa801c16389ebdd4a5df1145139c4a0bfe53ed4
SanghyeonLee <hyeon012366@gmail.com>
2025-05-06 20:14:49 +0900

    fix: 홈 템플릿 수정

--- src/components/templates/HomeTemplate.tsx
+++ src/components/templates/HomeTemplate.tsx
@@ -1,10 +1,8 @@
-// components/templates/HomeTemplate.tsx
 'use client';
 
-export default function HomeTemplate() {
-  return (
-    <div className="flex-center min-h-screen">
-      <h1 className="text-2xl font-notosanskr">안녕하세요 폰트 적용 테스트 이상현</h1>
-    </div>
-  );
+import { HomeTemplateProps } from '@/types/components/HomeTemplate';
+
+export default function HomeTemplate({ posts }: HomeTemplateProps) {
+  console.log(posts);
+  return <ul>{posts?.map((post) => <li key={post.id}>{post.title}</li>)}</ul>;
 }
