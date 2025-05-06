4c2aefb74bf6b1aca704991c9b869f4b323bdbca
SanghyeonLee <hyeon012366@gmail.com>
2025-05-06 21:10:25 +0900

    style: 임시 코드 주석

--- src/components/templates/HomeTemplate.tsx
+++ src/components/templates/HomeTemplate.tsx
@@ -4,5 +4,6 @@ import { HomeTemplateProps } from '@/types/components/HomeTemplate';
 
 export default function HomeTemplate({ posts }: HomeTemplateProps) {
   console.log(posts);
-  return <ul>{posts?.map((post) => <li key={post.id}>{post.title}</li>)}</ul>;
+  return <></>;
+  // <ul>{posts?.map((post) => <li key={post.id}>{post.title}</li>)}</ul>;
 }
