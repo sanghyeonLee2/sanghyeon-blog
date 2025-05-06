9a75a1ee9897a2a915eff608016a575a88b750b5
SanghyeonLee <hyeon012366@gmail.com>
2025-05-06 20:12:53 +0900

    chore: jest 설정 수정

--- jest.config.ts
+++ jest.config.ts
@@ -4,11 +4,15 @@ import type { Config } from 'jest';
 const config: Config = {
   testEnvironment: 'jsdom',
   preset: 'ts-jest',
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest', // JSX/TSX 파일 처리
+  },
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   testMatch: ['**/*.test.ts', '**/*.test.tsx'],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
 
