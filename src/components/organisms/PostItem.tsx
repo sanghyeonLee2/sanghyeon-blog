7f9d9004223efaa2f406fb86d9672cca027153d4
SanghyeonLee <hyeon012366@gmail.com>
2025-05-09 03:28:07 +0900

    feat: 포스트 하나의 컴포넌트 링크 걸기 및 구조 수정

--- src/components/organisms/PostItem.tsx
+++ src/components/organisms/PostItem.tsx
@@ -1,10 +1,29 @@
 // PostItem.tsx
 'use client';
 import React from 'react';
-import { PostItemProps } from '@/types/props/postItemsProps';
+import Thumbnail from '../atoms/Thumbnail';
+import Tag from '../atoms/Tag';
+import Summary from '../atoms/Summary';
+import Link from 'next/link';
+import { ROUTES } from '@/constants/routes';
+import { PostItemProps } from '@/types/props/postItemProps';
 
 const PostItem = ({ post }: PostItemProps) => {
-  return <li>{post.title}</li>;
+  return (
+    <Link href={ROUTES.POST(post.slug)}>
+      <article className="min-h-80">
+        <Thumbnail src={post.cover} />
+        <h2>{post.title}</h2>
+        <Summary text={post.summary} />
+        <div className="flex gap-1 my-3">
+          {post.tags.map((tag) => (
+            <Tag key={tag.id} name={tag.name} />
+          ))}
+        </div>
+        <span className="date">{post.date}</span>
+      </article>
+    </Link>
+  );
 };
 
 export default PostItem;
