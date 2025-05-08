98ba7ad8f5611a87a154de989d4011e2931e9d1d
SanghyeonLee <hyeon012366@gmail.com>
2025-05-09 03:24:57 +0900

    refactor: 불필요 코드 제거 및 로깅 추가

--- src/components/boundaries/LocalErrorBoundary.tsx
+++ src/components/boundaries/LocalErrorBoundary.tsx
@@ -1,15 +1,19 @@
 'use client';
 
 import { ErrorBoundary as ReactErrorBoundary } from 'react-error-boundary';
-import type { ReactNode } from 'react';
 import FallbackUI from '../organisms/FallbackUI';
 
-export default function LocalErrorBoundary({ children }: { children: ReactNode }) {
+export default function LocalErrorBoundary({ children }: { children: React.ReactNode }) {
+  console.log('boundary error');
   return (
     <ReactErrorBoundary
       fallbackRender={({ error, resetErrorBoundary }) => (
         <FallbackUI error={error} onRetry={resetErrorBoundary} />
       )}
+      onError={(error, info) => {
+        console.error('Local Error:', error);
+        console.error('Stack Trace:', info?.componentStack);
+      }}
     >
       {children}
     </ReactErrorBoundary>
