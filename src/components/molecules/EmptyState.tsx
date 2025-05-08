93d593b1fa9386a2fd86e8658a003aa271526fb8
SanghyeonLee <hyeon012366@gmail.com>
2025-05-09 03:26:11 +0900

    style: 변수 이름 수정

--- src/components/molecules/EmptyState.tsx
+++ src/components/molecules/EmptyState.tsx
@@ -1,8 +1,8 @@
-type Props = {
+type MassageProps = {
   message?: string;
 };
 
-export default function EmptyState({ message = '표시할 항목이 없습니다.' }: Props) {
+export default function EmptyState({ message = '표시할 항목이 없습니다.' }: MassageProps) {
   return (
     <div className="p-6 text-center text-gray-400">
       <p>{message}</p>
