2cb3b79351840564e049cccf868ef20eaf3c9f79
SanghyeonLee <hyeon012366@gmail.com>
2025-05-09 03:28:34 +0900

    feat: 포스트 map 추가

--- src/components/templates/HomeTemplate.tsx
+++ src/components/templates/HomeTemplate.tsx
@@ -1,9 +1,18 @@
 'use client';
-
-import { HomeTemplateProps } from '@/types/components/HomeTemplate';
+import LocalErrorBoundary from '../boundaries/LocalErrorBoundary';
+import PostItem from '../organisms/PostItem';
+import { HomeTemplateProps } from '@/types/props/homeTemplateProps';
 
 export default function HomeTemplate({ posts }: HomeTemplateProps) {
-  console.log(posts);
-  return <></>;
-  // <ul>{posts?.map((post) => <li key={post.id}>{post.title}</li>)}</ul>;
+  return (
+    <main className="w-full px-6">
+      <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6">
+        {posts?.map((post) => (
+          <LocalErrorBoundary key={post.id}>
+            <PostItem post={post} />
+          </LocalErrorBoundary>
+        ))}
+      </section>
+    </main>
+  );
 }
