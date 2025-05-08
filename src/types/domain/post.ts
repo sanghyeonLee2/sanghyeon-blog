8979c83af8a41766cfc2f445a5a906f3b38502d8
SanghyeonLee <hyeon012366@gmail.com>
2025-05-09 03:40:00 +0900

    feat: 응답 타입에 맞춰 타입 생성

--- src/types/domain/post.ts
+++ src/types/domain/post.ts
@@ -1,9 +1,69 @@
 // types/post.ts
 export interface Post {
-  id: number;
-  title: string;
-  summary: string;
-  content: string;
-  author: string;
+  id: string;
+  slug: string;
   date: string;
+  cover: string | null;
+  summary: string;
+  tags: MultiSelectTags[];
+  published: boolean;
+  title: string;
 }
+
+export interface NotionPostsResponse {
+  results: {
+    id: string;
+    properties: {
+      slug: Slug;
+      date: PostDate;
+      cover: Cover;
+      summary: Summary;
+      tags: Tags;
+      published: Published;
+      title: Title;
+    };
+  }[];
+}
+export interface Slug {
+  formula: {
+    type: 'string';
+    string: string;
+  };
+}
+export interface PostDate {
+  date: {
+    start: string;
+  };
+}
+export interface Cover {
+  files: {
+    name: string;
+    file: {
+      url: string;
+    };
+  }[];
+}
+
+export interface Summary {
+  rich_text: {
+    plain_text: string;
+  }[];
+}
+export interface Tags {
+  multi_select: MultiSelectTags[];
+}
+export interface MultiSelectTags {
+  id: string;
+  name: string;
+}
+export interface Published {
+  checkbox: boolean;
+}
+export interface Title {
+  title: {
+    plain_text: string;
+  }[];
+}
+export type PostSlugParam = {
+  slug: string;
+};
