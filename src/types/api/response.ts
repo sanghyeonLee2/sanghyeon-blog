ffc99d9215bb262dc948b55ca82a49a5d9260136
SanghyeonLee <hyeon012366@gmail.com>
2025-05-09 03:38:55 +0900

    feat: 타입 추가

--- src/types/api/response.ts
+++ src/types/api/response.ts
@@ -1,5 +1,7 @@
+import { notion } from '@/lib/notion/notionClient';
 export interface ApiResponse<T> {
   data: T;
   status: number;
   message?: string;
 }
+export type PostRecordMap = Awaited<ReturnType<typeof notion.getPage>>;
