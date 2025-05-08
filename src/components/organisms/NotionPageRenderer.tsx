1d3b6ce2310cd1008018dab330e7545f94f25668
SanghyeonLee <hyeon012366@gmail.com>
2025-05-09 03:53:50 +0900

    refactor: Collection 추가로 경고 메세지 제거

--- src/components/organisms/NotionPageRenderer.tsx
+++ src/components/organisms/NotionPageRenderer.tsx
@@ -5,6 +5,7 @@ import 'react-notion-x/src/styles.css';
 import 'prismjs/themes/prism-tomorrow.css';
 import { PostRecordMap } from '@/types/api/response';
 import { Code } from 'react-notion-x/build/third-party/code';
+import { Collection } from 'react-notion-x/build/third-party/collection';
 
 export default function NotionPageRenderer({ recordMap }: { recordMap: PostRecordMap }) {
   return (
@@ -16,6 +17,7 @@ export default function NotionPageRenderer({ recordMap }: { recordMap: PostRecor
         defaultPageCover="/default-cover.png"
         components={{
           Code,
+          Collection,
         }}
       />
     </div>
