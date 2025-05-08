99d24bb109c4b48dc189c62c7d77eabd9b69d25c
SanghyeonLee <hyeon012366@gmail.com>
2025-05-09 03:23:09 +0900

    refactor: 상수사용

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,9 +1,10 @@
 // components/atoms/Logo.tsx
+import { ROUTES } from '@/constants/routes';
 import Link from 'next/link';
 
 export default function Logo() {
   return (
-    <Link href="/" className="text-xl font-bold text-primary">
+    <Link href={ROUTES.HOME} className="text-xl font-bold text-primary">
       Sanghyeon
     </Link>
   );
