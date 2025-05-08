38187c55cd23779f8082790e32ff5966b6dcc039
SanghyeonLee <hyeon012366@gmail.com>
2025-05-09 03:22:51 +0900

    feat: 게시글 상세 페이지 slug 기반 추가

---
+++ src/app/posts/[slug]/page.tsx
@@ -0,0 +1,19 @@
+// app/posts/[slug]/page.tsx
+import { getPostBySlug } from '@/services/post/post';
+import 'react-notion-x/src/styles.css'; // 기본 스타일
+import 'prismjs/themes/prism-tomorrow.css'; // 코드 블록 스타일 (선택)
+import NotionPageRenderer from '@/components/organisms/NotionPageRenderer';
+import { getPostList } from '@/services/post/post';
+import { getPublishedSlugParams } from '@/services/post/utils';
+import type { PostSlugParam } from '@/types/domain/post';
+
+export default async function PostPage({ params }: { params: PostSlugParam }) {
+  const { slug } = await params;
+  const recordMap = await getPostBySlug(slug);
+  return <NotionPageRenderer recordMap={recordMap} />;
+}
+
+export async function generateStaticParams(): Promise<PostSlugParam[]> {
+  const posts = await getPostList();
+  return getPublishedSlugParams(posts);
+}
