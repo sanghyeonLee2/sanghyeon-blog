aea82d71ed0cf8f66d0007121ff8f739661862ab
SanghyeonLee <hyeon012366@gmail.com>
2025-05-09 03:22:23 +0900

    refactor: 불필요한 코드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
 // app/page.tsx
 import HomeTemplate from '@/components/templates/HomeTemplate';
-import { fetcher } from '@/lib/api/fetcher';
-import { API_URLS } from '@/constants/apiUrls';
-import { Post } from '@/types/domain/post';
+import { getPostList } from '@/services/post/post';
 
 export default async function Home() {
-  try {
-    const posts = await fetcher.post<Post[]>(API_URLS.POST.ALL, {});
-    return <HomeTemplate posts={posts} />;
-  } catch (err) {
-    console.error('❌ 에러:', err);
-    return <div>서버 에러 발생</div>;
-  }
+  const posts = await getPostList();
+  return <HomeTemplate posts={posts} />;
 }
