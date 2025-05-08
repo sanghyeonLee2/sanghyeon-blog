0b7a16eb6649a32f790305d8e350caa3f0445780
SanghyeonLee <hyeon012366@gmail.com>
2025-05-09 03:34:05 +0900

    rename: 파일 이름 수정

---
+++ src/lib/api/server/request.server.ts
@@ -0,0 +1,60 @@
+import { CONFIG } from '@/constants/config';
+import type { HttpMethod, FetcherOptions } from '@/types/api/fetchers';
+import { parseErrorResponse } from '../utils/parseErrorResponse';
+import { isCritical } from '@/utils/typeGuards';
+import showErrorToast from '../../ui/showErrorToast';
+const baseURL = process.env.NEXT_PUBLIC_NOTION_API_URL;
+const NOTION_TOKEN = process.env.NEXT_PUBLIC_NOTION_TOKEN;
+import { createCustomError } from '@/lib/api/utils/createCustomError';
+
+if (!baseURL || !NOTION_TOKEN) {
+  throw createCustomError(500);
+}
+
+const buildURL = (url: string) => `${baseURL}${url}`;
+
+async function request<T>(
+  method: HttpMethod,
+  url: string,
+  data?: unknown,
+  options: FetcherOptions = {},
+): Promise<T> {
+  const { timeout = CONFIG.DEFAULT_TIMEOUT, headers, ...rest } = options;
+  const controller = new AbortController();
+
+  const finalHeaders: HeadersInit = {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+    ...(headers || {}),
+    Authorization: `Bearer ${NOTION_TOKEN}`,
+    'Notion-Version': '2022-06-28',
+  };
+
+  const fetchOptions: RequestInit = {
+    method,
+    headers: finalHeaders,
+    signal: controller.signal,
+    ...rest,
+    body: data ? JSON.stringify(data) : undefined,
+  };
+
+  const timeoutId = setTimeout(() => controller.abort(), timeout);
+
+  try {
+    const res = await fetch(buildURL(url), fetchOptions);
+
+    if (!res.ok) {
+      const parsedError = await parseErrorResponse(res);
+
+      if (isCritical(parsedError)) throw createCustomError(parsedError.status, parsedError.message);
+
+      showErrorToast(parsedError.status);
+    }
+
+    return await res.json();
+  } finally {
+    clearTimeout(timeoutId);
+  }
+}
+
+export default request;
