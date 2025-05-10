0fe395e2033e675b1f5bf23ceb4eb8f0b449aa2c
SanghyeonLee <hyeon012366@gmail.com>
2025-05-10 19:22:29 +0900

    design: 헤더 디자인 수정

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -6,7 +6,7 @@ import Logo from '../atoms/Logo';
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between max-w-4xl w-full z-50 h-18 section-padding">
+    <header className="flex items-center justify-between w-full z-50 h-18 p-content">
       <Logo />
       <ThemeToggle />
     </header>
