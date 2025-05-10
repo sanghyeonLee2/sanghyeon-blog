c637d4ea03265e636359beefbfac1736e4592724
SanghyeonLee <hyeon012366@gmail.com>
2025-05-10 19:19:30 +0900

    feat: 푸터 레이아웃에 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,16 +3,17 @@ import '@/styles/globals.css';
 import { pretendard } from '@/fonts/fonts';
 import { cookies } from 'next/headers';
 import Header from '@/components/organisms/Header';
+import Footer from '@/components/organisms/Footer';
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const cookieStore = await cookies();
   const theme = cookieStore.get('theme')?.value === 'dark' ? 'dark' : '';
-
   return (
     <html lang={CONFIG.DEFAULT_LANGUAGE} className={`${pretendard.variable} ${theme}`}>
       <body className={`${pretendard.className} layout-wrapper`}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
