dd8cd0fa4ff2872dd16416583269a42b35f1d742
SanghyeonLee <hyeon012366@gmail.com>
2025-05-10 19:23:35 +0900

    style: main 태그 안에 banner 오게 수정

--- src/components/templates/HomeTemplate.tsx
+++ src/components/templates/HomeTemplate.tsx
@@ -1,12 +1,14 @@
 'use client';
 import LocalErrorBoundary from '../boundaries/LocalErrorBoundary';
+import Banner from '../organisms/Banner';
 import PostItem from '../organisms/PostItem';
 import { HomeTemplateProps } from '@/types/props/homeTemplateProps';
 
 export default function HomeTemplate({ posts }: HomeTemplateProps) {
   return (
-    <main className="w-full px-6">
-      <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6">
+    <main className="w-full px-content flex-grow">
+      <Banner />
+      <section className="py-content grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6">
         {posts?.map((post) => (
           <LocalErrorBoundary key={post.id}>
             <PostItem post={post} />
