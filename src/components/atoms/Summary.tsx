f452fb11344ce723011da6c051ddf40d1967615e
SanghyeonLee <hyeon012366@gmail.com>
2025-05-10 19:20:54 +0900

    design: 라인 ...  3줄 넘을때로 수정

--- src/components/atoms/Summary.tsx
+++ src/components/atoms/Summary.tsx
@@ -5,7 +5,7 @@ interface SummaryProps {
 }
 
 const Summary = ({ text }: SummaryProps) => {
-  return <p className="line-clamp-2 ...">{text}</p>;
+  return <p className="line-clamp-3 ... hover-opacity">{text}</p>;
 };
 
 export default Summary;
