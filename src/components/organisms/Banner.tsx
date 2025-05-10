32a05fc358882055eb6d6e40703e9bf2be6918d2
SanghyeonLee <hyeon012366@gmail.com>
2025-05-10 19:22:04 +0900

    feat: 배너 추가

---
+++ src/components/organisms/Banner.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export default function Banner() {
+  return (
+    <div className="w-full flex justify-center">
+      <div className="rounded-md shadow-[var(--shadow)] bg-[var(--color-primary)] p-content text-center text-sm font-medium w-full">
+        🚀 다크모드, 라이트모드 마스터, 스켈레톤 UI는 일단 고려
+      </div>
+    </div>
+  );
+}
