472d05587b5f287404f7c69d189ee341ea15d207
SanghyeonLee <hyeon012366@gmail.com>
2025-05-10 19:20:09 +0900

    style: 로고 이미지 수정

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 
 export default function Logo() {
   return (
-    <Link href={ROUTES.HOME} className="text-xl font-bold text-primary">
-      Sanghyeon
+    <Link href={ROUTES.HOME} className="hover:opacity-80">
+      <h1 className="text-3xl font-mono font-bold italic tracking-[0.05em]">SH.LOG</h1>
     </Link>
   );
 }
