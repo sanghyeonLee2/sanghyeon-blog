18b6cc9aea7b1563f38af4290f75e87e0746d843
SanghyeonLee <hyeon012366@gmail.com>
2025-05-10 19:21:11 +0900

    design: 테그 다크모드 적용

--- src/components/atoms/Tag.tsx
+++ src/components/atoms/Tag.tsx
@@ -9,8 +9,9 @@ const Tag = ({ name }: TagProps) => {
   return (
     <span
       className="
-    bg-gray-200 text-gray-800
-    dark:bg-gray-700 dark:text-gray-100
+      bg-[var(--color-primary)]
+      shadow-[var(--shadow)]
+      hover-opacity
     text-xs px-2 py-1 rounded-2xl
   "
     >
