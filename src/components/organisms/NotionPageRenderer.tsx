b8d1e697d351e98b6ed3c47b50c8fb12b9a03516
SanghyeonLee <hyeon012366@gmail.com>
2025-05-11 23:53:50 +0900

    refactor: vercel 성능 및 비용 최적화 위한 불필요 속성 제거

--- src/components/organisms/NotionPageRenderer.tsx
+++ src/components/organisms/NotionPageRenderer.tsx
@@ -1,12 +1,20 @@
 // components/organisms/NotionPageRenderer.tsx
 'use client';
+
+import dynamic from 'next/dynamic';
 import { NotionRenderer } from 'react-notion-x';
 import { PostRecordMap } from '@/types/api/response';
-import { Code } from 'react-notion-x/build/third-party/code';
 import { useTheme } from '@/hooks/useTheme';
+import NotionImage from './../atoms/NotionImage';
+
+// ✅ Code 컴포넌트 lazy import
+const Code = dynamic(() => import('react-notion-x/build/third-party/code').then((m) => m.Code), {
+  ssr: false, // Next.js에서 클라이언트에서만 로드되도록 설정
+});
 
 export default function NotionPageRenderer({ recordMap, id }: PostRecordMap) {
   const { isDark } = useTheme();
+
   return (
     <div>
       <NotionRenderer
@@ -15,9 +23,10 @@ export default function NotionPageRenderer({ recordMap, id }: PostRecordMap) {
         disableHeader={true}
         recordMap={recordMap}
         darkMode={isDark}
-        previewImages={true}
+        forceCustomImages={true}
         components={{
           Code,
+          Image: NotionImage,
         }}
       />
     </div>
