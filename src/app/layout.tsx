856c293f34577ed5fbf9ad3254cf04c2e4dcfbfe
SanghyeonLee <hyeon012366@gmail.com>
2025-05-13 04:40:08 +0900

    fix: 다크모드 관련 재 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-// app/layout.tsx 또는 app/global.css에 직접 import
 import { CONFIG } from '@/constants/config';
 import '@/styles/globals.css';
 import { pretendard } from '@/fonts/fonts';
@@ -10,8 +9,8 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   const cookieStore = await cookies();
   const theme = cookieStore.get('theme')?.value === 'dark' ? 'dark' : '';
   return (
-    <html lang={CONFIG.DEFAULT_LANGUAGE} className={`${pretendard.variable}`}>
-      <body className={`${pretendard.className} layout-wrapper ${theme}`}>
+    <html lang={CONFIG.DEFAULT_LANGUAGE} className={`${pretendard.variable} ${theme}`}>
+      <body className={`${pretendard.className} layout-wrapper`}>
         <Header />
         {children}
         <Footer />
--- src/stores/themeStore.ts
+++ src/stores/themeStore.ts
@@ -8,20 +8,19 @@ type ThemeState = {
   toggleTheme: () => void;
 };
 
-// themeStore.ts
 export const useThemeStore = create<ThemeState>((set, get) => ({
-  theme: 'dark',
+  theme: 'dark', // SSR-safe 기본값
   setTheme: (theme) => {
     set({ theme });
-    const body = document.body;
-    body.classList.remove('dark');
-    if (theme === 'dark') body.classList.add('dark');
+    const root = document.documentElement;
+    root.classList.remove('dark');
+    if (theme === 'dark') root.classList.add('dark');
     document.cookie = `theme=${theme}; path=/`;
   },
   toggleTheme: () => {
     const newTheme = get().theme === 'dark' ? 'light' : 'dark';
     set({ theme: newTheme });
-    document.body.classList.toggle('dark', newTheme === 'dark');
+    document.documentElement.classList.toggle('dark', newTheme === 'dark');
     document.cookie = `theme=${newTheme}; path=/`;
   },
 }));
