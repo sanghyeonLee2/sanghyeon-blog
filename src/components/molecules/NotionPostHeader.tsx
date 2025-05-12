a3882b83e1b8e40116a00c19d900b42b1fa25b6b
SanghyeonLee <hyeon012366@gmail.com>
2025-05-12 23:50:54 +0900

    feat: 노션 커스텀 헤더 추가

---
+++ src/components/molecules/NotionPostHeader.tsx
@@ -0,0 +1,24 @@
+import { MultiSelectTags } from '@/types/domain/post';
+import PostMeta from './PostMeta';
+import { JSX } from 'react';
+
+type NotionPostHeaderProps = {
+  title: string;
+  postedDate: string;
+  tags: MultiSelectTags[];
+};
+
+export default function NotionPostHeader({
+  title,
+  postedDate,
+  tags,
+}: NotionPostHeaderProps): JSX.Element {
+  return (
+    <div>
+      <div className="pt-5 pb-2">
+        <h1 className="py-4 text-4xl font-bold leading-snug tracking-tight">{title}</h1>
+        <PostMeta tags={tags} date={postedDate} />
+      </div>
+    </div>
+  );
+}
