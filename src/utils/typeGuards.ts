361b4583bada4a2ad9fdfc8b993dbb700978901c
SanghyeonLee <hyeon012366@gmail.com>
2025-05-12 23:54:42 +0900

    feat: getCoverUrl 코드 추가

--- src/utils/typeGuards.ts
+++ src/utils/typeGuards.ts
@@ -1,4 +1,5 @@
 import type { CustomError } from '@/types/api/errors';
+import { File, ExternalFile } from './../types/domain/post';
 
 export function isCustomError(error: unknown): error is CustomError {
   return (
@@ -14,4 +15,19 @@ export function isCritical(error: unknown): boolean {
 
   const status = error.status;
   return status === 500 || status === 403;
+}
+
+export function getCoverUrl(coverFile: (File | ExternalFile)[]): string | null {
+  if (coverFile.length === 0) {
+    return null;
+  }
+  const firstCoverFile = coverFile.at(0);
+  if (firstCoverFile?.type === 'external') {
+    return (firstCoverFile as ExternalFile).external.url;
+  }
+  if (firstCoverFile?.type === 'file') {
+    return (firstCoverFile as File).file.url;
+  }
+
+  return null;
 }
