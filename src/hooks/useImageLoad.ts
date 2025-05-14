2364d45ed03885aad5dc489acff4fecbe467047e
SanghyeonLee <hyeon012366@gmail.com>
2025-05-15 03:54:40 +0900

    feat: 스켈레톤 이미지 구현 위한 이미지 로드 관련 커스텀 훅 추가

---
+++ src/hooks/useImageLoad.ts
@@ -0,0 +1,26 @@
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
+
+type UseLoadImageReturn = {
+  setLoadSuccess: () => void;
+  loaded: boolean;
+  imgRef: React.RefObject<HTMLImageElement | null>;
+};
+
+const useImageLoad = (): UseLoadImageReturn => {
+  const [loaded, setLoaded] = useState(false);
+  const imgRef = useRef<HTMLImageElement>(null);
+
+  useLayoutEffect(() => {
+    if (imgRef.current?.complete) {
+      setLoaded(true);
+    }
+  }, []);
+
+  return {
+    setLoadSuccess: () => setLoaded(true),
+    loaded,
+    imgRef,
+  };
+};
+
+export default useImageLoad;
