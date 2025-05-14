a71e5b00b431646986d70294e73dd510bdbf8718
SanghyeonLee <hyeon012366@gmail.com>
2025-05-15 03:53:59 +0900

    rename: 불필요 코드 제거

--- src/components/templates/NotionPost.tsx
+++ src/components/templates/NotionPost.tsx
@@ -3,18 +3,15 @@
 import dynamic from 'next/dynamic';
 import { NotionRenderer } from 'react-notion-x';
 import { PostRecordMap } from '@/types/api/response';
-import NotionImage from '../atoms/image/NotionImage';
+import NotionImage from '../molecules/image/NotionImage';
 import NotionPostHeader from '../molecules/post/NotionPostHeader';
 import Giscus from '../organisms/post/Giscus';
-import { useNextTheme } from '@/hooks/useNextTheme';
 
 const Code = dynamic(() => import('react-notion-x/build/third-party/code').then((m) => m.Code), {
   ssr: false,
 });
 
 export default function NotionPost({ additionalPostData, recordMap }: PostRecordMap) {
-  const { isDark } = useNextTheme();
-
   return (
     <div className="pb-20">
       {/* <CoverImage alt={'썸네일'} src={additionalPostData.cover} className="h-[30vh]" /> */}
@@ -24,7 +21,6 @@ export default function NotionPost({ additionalPostData, recordMap }: PostRecord
         disableHeader={true}
         recordMap={recordMap}
         pageTitle={<></>}
-        darkMode={isDark}
         forceCustomImages={true}
         showTableOfContents={true}
         components={{
