49baca7a19d1db0d15df92bfdc633127d08ff635
SanghyeonLee <hyeon012366@gmail.com>
2025-05-15 20:15:19 +0900

    refactor: 푸터 상수 교체

---
+++ src/constants/metaData.ts
@@ -0,0 +1,10 @@
+export const SITE_METADATA = {
+  BLOG_INTRO: 'SH.LOG',
+  BLOG_DESCRIPTION: 'Learning from building',
+  COPY_RIGHT: '© 2025 Sanghyeon Lee. All rights reserved.',
+  MY_GITHUB_URL: 'https://github.com/sanghyeonLee2',
+  LINK_TO_GITHUB: 'View on GitHub ↗',
+  AUTHOR: 'Sanghyeon Lee',
+  SITE_NAME: 'SH.LOG',
+  DESCRIPTION: '개발 블로그입니다.',
+};
