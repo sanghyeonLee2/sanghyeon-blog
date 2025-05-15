d8915ca7d7400dd9e953d08d3634a87eca440455
SanghyeonLee <hyeon012366@gmail.com>
2025-05-15 20:14:21 +0900

    design: 로고 스타일 수정

--- src/components/atoms/Logo/Logo.tsx
+++ src/components/atoms/Logo/Logo.tsx
@@ -1,10 +1,11 @@
+import { SITE_METADATA } from '@/constants/metaData';
 import { ROUTES } from '@/constants/routes';
 import Link from 'next/link';
 
 export default function Logo() {
   return (
     <Link href={ROUTES.HOME} className="hover:opacity-80">
-      <h1 className="text-3xl font-mono font-bold italic tracking-[0.05em]">SH.LOG</h1>
+      <h1 className="text-2xl font-bold tracking-[0.05em]">{SITE_METADATA.SITE_NAME}</h1>
     </Link>
   );
 }
