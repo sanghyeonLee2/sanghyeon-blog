c6412f0754e914a0638263ba133c7ba9b2fc2d99
SanghyeonLee <hyeon012366@gmail.com>
2025-05-17 02:34:39 +0900

    style: 코드 정렬

--- src/components/organisms/post/Giscus.tsx
+++ src/components/organisms/post/Giscus.tsx
@@ -4,7 +4,5 @@ import useGetGiscus from '@/hooks/useGetGiscus';
 
 export default function Giscus() {
   const giscusRef = useGetGiscus();
-  return (
-    <section ref={giscusRef} className="px-content min-h-[28.125rem] transition-all duration-300" />
-  );
+  return <section ref={giscusRef} className="min-h-[28.125rem] transition-all duration-300" />;
 }
