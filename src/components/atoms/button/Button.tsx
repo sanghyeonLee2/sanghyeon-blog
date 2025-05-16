3e3d28922fed529fa8b8fcd2d37cec9b69438320
SanghyeonLee <hyeon012366@gmail.com>
2025-05-17 02:33:57 +0900

    design: 버튼 스타일 추가

--- src/components/atoms/button/Button.tsx
+++ src/components/atoms/button/Button.tsx
@@ -15,7 +15,7 @@ export default function Button({
       type={type}
       onClick={onClick}
       className={clsx(
-        'hover:opacity-85 text-gray-100 text-base font-medium px-4 py-2 rounded bg-[var(--color-button-bg)] transition',
+        'hover:opacity-85 text-[var(--color-button-text)] text-base font-medium px-4 py-2 rounded bg-[var(--color-button-bg)] transition',
         className,
       )}
       {...rest}
