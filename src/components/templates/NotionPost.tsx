c0f19d9548a64529bb475c95fdbb04d9e8d618e9
SanghyeonLee <hyeon012366@gmail.com>
2025-05-17 02:34:51 +0900

    feat: 목록 돌아가기 버튼 추가

--- src/components/templates/NotionPost.tsx
+++ src/components/templates/NotionPost.tsx
@@ -6,12 +6,16 @@ import { PostRecordMap } from '@/types/api/response';
 import NotionImage from '../molecules/image/NotionImage';
 import NotionPostHeader from '../molecules/post/NotionPostHeader';
 import Giscus from '../organisms/post/Giscus';
+import Button from '../atoms/button/Button';
+import { useRouter } from 'next/navigation';
+import { ROUTES } from '@/constants/routes';
 
 const Code = dynamic(() => import('react-notion-x/build/third-party/code').then((m) => m.Code), {
   ssr: false,
 });
 
 export default function NotionPost({ additionalPostData, recordMap }: PostRecordMap) {
+  const router = useRouter();
   return (
     <div className="pb-20">
       {/* <CoverImage alt={'썸네일'} src={additionalPostData.cover} className="h-[30vh]" /> */}
@@ -35,6 +39,11 @@ export default function NotionPost({ additionalPostData, recordMap }: PostRecord
             postedDate={additionalPostData.postedDate}
           />
         }
+      />
+      <Button
+        text="목록으로 돌아가기"
+        className="mb-6 w-full"
+        onClick={() => router.push(ROUTES.HOME)}
       />
       <Giscus />
     </div>
