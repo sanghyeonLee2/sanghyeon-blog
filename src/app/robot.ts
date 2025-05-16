a85a16623720a10a6c42a07f303b0b14007b1976
SanghyeonLee <hyeon012366@gmail.com>
2025-05-17 01:52:45 +0900

    feat: 검색엔진에 url 알려주는 로직 추가

--- src/app/robot.ts
+++ src/app/robot.ts
@@ -9,5 +9,6 @@ export default function robots(): MetadataRoute.Robots {
       disallow: '/private/',
     },
     sitemap: `${SITE_METADATA.VERCEL_DEPLOY_URL}/sitemap.xml`,
+    host: SITE_METADATA.VERCEL_DEPLOY_URL,
   };
 }
