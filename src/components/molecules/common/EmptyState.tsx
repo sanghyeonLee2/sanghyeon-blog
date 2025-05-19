7cdd4ba7900db3eaaa9afb47ec0fac5b4b1dd61a
SanghyeonLee <hyeon012366@gmail.com>
2025-05-19 21:42:22 +0900

    feat: 포스트 없을시 항목없음 표시

--- src/components/molecules/common/EmptyState.tsx
+++ src/components/molecules/common/EmptyState.tsx
@@ -4,8 +4,8 @@ type MassageProps = {
 
 export default function EmptyState({ message = '표시할 항목이 없습니다.' }: MassageProps) {
   return (
-    <div className="p-6 text-center text-gray-400">
-      <p>{message}</p>
+    <div className="p-6 h-[60vh] flex-center text-gray-400">
+      <h2>{message}</h2>
     </div>
   );
 }
--- src/components/templates/Home.tsx
+++ src/components/templates/Home.tsx
@@ -2,14 +2,19 @@
 import { Post } from '@/types/domain/post';
 import PostItem from '../organisms/post/PostItem';
 import Banner from '../molecules/ui/Banner';
+import EmptyState from '../molecules/common/EmptyState';
 
 export default function Home({ posts }: { posts: Post[] }) {
   return (
     <>
       <Banner />
-      <section className="py-content grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4.75">
-        {posts?.map((post) => <PostItem post={post} key={post.id} />)}
-      </section>
+      {posts.length === 0 ? (
+        <EmptyState />
+      ) : (
+        <section className="py-content grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4.75">
+          {posts?.map((post) => <PostItem post={post} key={post.id} />)}
+        </section>
+      )}
     </>
   );
 }
