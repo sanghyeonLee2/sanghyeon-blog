72e301617891abbdf4868398d877c3c2a043f78e
SanghyeonLee <hyeon012366@gmail.com>
2025-06-04 19:44:43 +0900

    design: 포스트 목록에서 제목 폰트 굵기 및 마진 바텀 증가

--- src/components/molecules/post/PostContent.tsx
+++ src/components/molecules/post/PostContent.tsx
@@ -9,7 +9,7 @@ interface PostContentProps {
 const PostContent = ({ title, summary }: PostContentProps): JSX.Element => {
   return (
     <div>
-      <h2 className="min-h-[3.5rem] text-xl font-semibold mb-1 line-clamp-2">{title}</h2>
+      <h2 className="min-h-[3.5rem] text-xl font-bold mb-2 line-clamp-2">{title}</h2>
       <p className="text-sm min-h-[3.75rem] text-muted-foreground mb-2 line-clamp-3">{summary}</p>
     </div>
   );
