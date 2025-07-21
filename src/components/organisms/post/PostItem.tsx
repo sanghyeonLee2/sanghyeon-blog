d9885a0f2954bbf4e6f3328616895fb9985ec202
SanghyeonLee <hyeon012366@gmail.com>
2025-07-22 00:36:05 +0900

    refactor: 포스트 디자인 수정

--- src/components/organisms/post/PostItem.tsx
+++ src/components/organisms/post/PostItem.tsx
@@ -4,27 +4,29 @@ import React, { JSX } from 'react';
 import Thumbnail from '../../atoms/image/Image';
 import Link from 'next/link';
 import { ROUTES } from '@/constants/routes';
-import PostContent from '../../molecules/post/PostContent';
-import PostMeta from '../../molecules/post/PostMeta';
 import { Post } from '@/types/domain/post';
 import { DEFAULTS } from '@/constants/defaults';
+import Tags from '@/components/molecules/post/Tags';
 
 const PostItem = ({ post }: { post: Post }): JSX.Element => {
   return (
-    <Link href={ROUTES.POST(post.slug)}>
-      <article className="bg-[var(--color-post-bg)] h-full flex flex-col rounded-2xl shadow-[var(--shadow)] transition hover:shadow-lg hover:-translate-y-1">
-        <Thumbnail
-          alt="커버이미지"
-          src={post.cover ?? DEFAULTS.IMG_URL}
-          className="aspect-[3/2] w-full rounded-t-2xl"
-        />
-        <div className="flex flex-col justify-between flex-1 px-4 py-4">
-          <PostContent title={post.title} summary={post.summary} />
-          <PostMeta tags={post.tags} date={post.date} />
-        </div>
-      </article>
+    <Link
+      href={ROUTES.POST(post.slug)}
+      className="rounded-2xl hover:bg-[var(--color-bg-hover)] transition-colors duration-300 ease-in-out p-4 -mx-4 flex sm:flex-row flex-col-reverse gap-4 justify-between sm:items-center"
+    >
+      <div className="flex flex-col gap-2">
+        <h2 className="text-xl font-bold line-clamp-2">{post.title}</h2>
+        <p className="text-sm text-muted-foreground line-clamp-2">{post.summary}</p>
+        <p className="text-sm text-gray-400">{post.date}</p>
+        <Tags tags={post.tags} />
+      </div>
+      <Thumbnail
+        alt="커버이미지"
+        src={post.cover ?? DEFAULTS.IMG_URL}
+        className="aspect-[3/2] w-full rounded-2xl sm:max-w-45 backdrop-blur-md border border-white/10 shadow-[var(--shadow)]"
+      />
     </Link>
   );
 };
 
-export default PostItem;
+export default React.memo(PostItem);
