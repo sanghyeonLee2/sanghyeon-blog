6c530b2220dcff6ff5d699c026c33816fedb33a7
SanghyeonLee <hyeon012366@gmail.com>
2025-07-22 00:34:04 +0900

    refactor: NotionPostHeader 컴포넌트 구조 개선 및 PostMeta 제거

--- src/components/molecules/post/NotionPostHeader.tsx
+++ src/components/molecules/post/NotionPostHeader.tsx
@@ -1,23 +1,36 @@
+'use client';
+
 import { MultiSelectTags } from '@/types/domain/post';
-import PostMeta from './PostMeta';
 import { JSX } from 'react';
+import Tags from '@/components/molecules/post/Tags';
+import Thumbnail from '@/components/atoms/image/Image';
+import { DEFAULTS } from '@/constants/defaults';
 
 type NotionPostHeaderProps = {
   title: string;
   postedDate: string;
+  cover: string | null;
   tags: MultiSelectTags[];
 };
-
 export default function NotionPostHeader({
   title,
   postedDate,
   tags,
+  cover,
 }: NotionPostHeaderProps): JSX.Element {
   return (
-    <div>
-      <div className="pb-2">
-        <h1 className="pb-4 text-4xl font-bold leading-snug tracking-tight">{title}</h1>
-        <PostMeta tags={tags} date={postedDate} />
+    <div className="w-full">
+      <h1 className="pb-3 text-4xl break-keep text-center font-bold leading-snug tracking-tight">
+        {title}
+      </h1>
+      <div className="flex flex-col items-center gap-5 py-5">
+        <Tags tags={tags} />
+        <Thumbnail
+          alt="커버이미지"
+          src={cover ?? DEFAULTS.IMG_URL}
+          className="aspect-[3/2] w-full max-w-xl rounded-2xl backdrop-blur-md border border-white/10 shadow-[var(--shadow)]"
+        />
+        <p className="text-sm text-gray-400">{postedDate}</p>
       </div>
     </div>
   );
