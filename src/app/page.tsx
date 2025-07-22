7d57425b4d241a08f5f28b49575240b2fa8415bc
SanghyeonLee <hyeon012366@gmail.com>
2025-07-22 23:04:00 +0900

    fix: 'dynamic' 변수를 'revalidate'로 변경하여 캐시 재검증 시간 설정 이미지 만료 로 인한 오류 대응

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-export const dynamic = 'force-static';
-
 import HomeTemplate from '@/components/templates/Home';
 import { getPostList } from '@/services/blogPost';
+
+export const revalidate = 3000;
 
 export default async function Home() {
   const posts = await getPostList();
