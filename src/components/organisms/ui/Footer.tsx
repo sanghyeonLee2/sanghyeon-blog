2b17296063e130eb3c563c365f4e63362501ed00
SanghyeonLee <hyeon012366@gmail.com>
2025-07-23 22:20:01 +0900

    fix: Footer 컴포넌트의 스타일 수정 및 GitHub 링크 아이콘 추가

--- src/components/organisms/ui/Footer.tsx
+++ src/components/organisms/ui/Footer.tsx
@@ -1,15 +1,19 @@
 'use client';
 
 import { SITE_METADATA } from '@/constants/metaData';
+import { Github as GithubIcon } from 'lucide-react';
 
 export default function Footer() {
   return (
-    <footer className="flex items-center justify-between w-full h-18 p-content">
+    <footer className="flex items-center justify-between w-full h-12 px-content py-3 mt-10">
       <small className="text-[0.625rem] sm:text-xs text-subtle">{SITE_METADATA.COPY_RIGHT}</small>
-      <a href={SITE_METADATA.MY_GITHUB_URL} className="hover-opacity flex items-center">
-        <small className="text-[0.625rem] sm:text-xs text-subtle">
-          {SITE_METADATA.LINK_TO_GITHUB}
-        </small>
+      <a
+        href={SITE_METADATA.MY_GITHUB_URL}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover-opacity"
+      >
+        <GithubIcon height={18} width={18} />
       </a>
     </footer>
   );
