1bfdff0ada8bd55492da0617093bc362b3e179c7
SanghyeonLee <hyeon012366@gmail.com>
2025-07-23 22:19:51 +0900

    fix: NotionPostHeader 컴포넌트의 제목 스타일 수정 및 Thumbnail 클래스 속성 변경

--- src/components/molecules/post/NotionPostHeader.tsx
+++ src/components/molecules/post/NotionPostHeader.tsx
@@ -20,15 +20,14 @@ export default function NotionPostHeader({
 }: NotionPostHeaderProps): JSX.Element {
   return (
     <div className="w-full">
-      <h1 className="pb-3 text-4xl break-keep text-center font-bold leading-snug tracking-tight">
-        {title}
-      </h1>
+      <h1 className="break-keep text-center leading-snug tracking-tight">{title}</h1>
       <div className="flex flex-col items-center gap-5 py-5">
         <Tags tags={tags} />
         <Thumbnail
           alt="커버이미지"
           src={cover ?? DEFAULTS.IMG_URL}
-          className="aspect-[3/2] w-full max-w-xl rounded-2xl backdrop-blur-md border border-white/10 shadow-[var(--shadow)]"
+          className="object-cover"
+          wrapClassName="aspect-[3/2] w-full max-w-xl rounded-2xl backdrop-blur-md border border-white/10 shadow-[var(--shadow)]"
         />
         <p className="text-sm text-gray-400">{postedDate}</p>
       </div>
