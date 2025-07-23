c13379d259c1ff64d109b92424b0836ea24b8721
SanghyeonLee <hyeon012366@gmail.com>
2025-07-23 22:19:39 +0900

    fix: Image 컴포넌트에 로딩 상태를 표시하는 스켈레톤 이미지 추가 및 클래스 구조 개선

--- src/components/atoms/image/Image.tsx
+++ src/components/atoms/image/Image.tsx
@@ -1,12 +1,33 @@
+'use client';
+
 import { DEFAULTS } from '@/constants/defaults';
 import React from 'react';
-import { ImageProps } from './types';
 import clsx from 'clsx';
+import useImageLoad from '@/hooks/useImageLoad';
+import SkeletonImage from '@/components/atoms/image/SkeletonImage';
+export interface ImageProps {
+  src: string;
+  className?: string;
+  wrapClassName?: string;
+  alt: string;
+}
 
-const Image = ({ src = '', className, alt }: ImageProps) => {
+const Image = ({ src = '', className, wrapClassName, alt }: ImageProps) => {
+  const { loaded, imgRef } = useImageLoad(src);
+
   return (
-    <div className={clsx('overflow-hidden', className)}>
-      <img src={src ?? DEFAULTS.IMG_URL} alt={alt} className="w-full h-full object-cover" />
+    <div className={clsx('relative overflow-hidden', wrapClassName)}>
+      {!loaded && <SkeletonImage />}
+      <img
+        ref={imgRef}
+        src={src ?? DEFAULTS.IMG_URL}
+        alt={alt}
+        className={clsx(
+          'w-full h-full transition-opacity duration-300',
+          loaded ? 'opacity-100' : 'opacity-0',
+          className,
+        )}
+      />
     </div>
   );
 };
