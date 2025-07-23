0d85faea948fe104b42cd4fc8a3716d3694de745
SanghyeonLee <hyeon012366@gmail.com>
2025-07-23 22:23:22 +0900

    rename: 불필요 코드 제거

--- src/constants/metaData.ts
+++ src/constants/metaData.ts
@@ -6,7 +6,6 @@ export const SITE_METADATA = {
   COPY_RIGHT: '© 2025 Sanghyeon Lee. All rights reserved.',
   MY_GITHUB_URL: 'https://github.com/sanghyeonLee2',
   VERCEL_DEPLOY_URL: 'https://sanghyeon-blog.vercel.app',
-  LINK_TO_GITHUB: 'View on GitHub ↗',
   AUTHOR: 'Sanghyeon Lee',
   SITE_NAME: 'SH.LOG',
   DESCRIPTION: '개발 블로그입니다.',
