74db5262369ddef422e258448248f3d6aa8ea83e
SanghyeonLee <hyeon012366@gmail.com>
2025-07-28 22:59:26 +0900

    refactor: HttpClient 인터페이스의 get 및 post 메서드에 revalidate 매개변수 추가

--- src/types/api/fetchers.ts
+++ src/types/api/fetchers.ts
@@ -1,11 +1,6 @@
 export type HttpMethod = 'GET' | 'POST';
 
-// export interface FetcherOptions extends Omit<RequestInit, 'method' | 'body'> {
-//   timeout?: number;
-//   headers?: HeadersInit;
-// }
-
 export interface HttpClient {
-  get: <T = unknown>(endpoint: string) => Promise<T>;
-  post: <T = unknown>(endpoint: string, data?: unknown) => Promise<T>;
+  get: <T = unknown>(endpoint: string, revalidate?: number | false) => Promise<T>;
+  post: <T = unknown>(endpoint: string, revalidate?: number | false) => Promise<T>;
 }
