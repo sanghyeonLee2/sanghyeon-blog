2b87c6b047dfb41c3a49d054a546bff0f1355d31
SanghyeonLee <hyeon012366@gmail.com>
2025-07-31 00:32:05 +0900

    refactor: 배너 비활성화 및 logs 텍스트 홈 화면 추가

--- src/components/templates/Home.tsx
+++ src/components/templates/Home.tsx
@@ -7,11 +7,11 @@ import EmptyState from '../molecules/common/EmptyState';
 export default function Home({ posts }: { posts: Post[] }) {
   return (
     <>
-      <Banner />
       {posts.length === 0 ? (
         <EmptyState />
       ) : (
-        <div className="pb-5rem grid grid-cols-1 gap-5">
+        <div className="py-[2rem] grid grid-cols-1 gap-5">
+          <h1 className="font-mono">Logs</h1>
           {posts?.map((post) => <PostItem post={post} key={post.id} />)}
         </div>
       )}
