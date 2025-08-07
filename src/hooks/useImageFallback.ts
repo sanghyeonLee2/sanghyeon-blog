0e55385179ec14e40464624b414f1d5eacaba5b4
SanghyeonLee <hyeon012366@gmail.com>
2025-08-07 17:14:50 +0900

    fix: useImageFallback 훅에서 src 매개변수 타입 변경 및 오류 처리 로직 수정

--- src/hooks/useImageFallback.ts
+++ src/hooks/useImageFallback.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { DEFAULTS } from '@/constants/defaults';
 import { API_URLS } from '@/constants/apiUrls';
 
-const useImageFallback = (src?: string) => {
+const useImageFallback = (src: string) => {
   const [presignedUrl, setPresignedUrl] = useState(DEFAULTS.IMG_URL);
 
   useEffect(() => {
@@ -14,7 +14,7 @@ const useImageFallback = (src?: string) => {
   const handleError = () => {
     console.error(`Image load failed for ${src}. Falling back to default image.`);
 
-    setPresignedUrl(API_URLS.GET.IMAGE_PROXY(DEFAULTS.IMG_URL));
+    setPresignedUrl(API_URLS.GET.IMAGE_PROXY(src) || DEFAULTS.IMG_URL);
   };
 
   return {
